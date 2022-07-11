6c3ac07e39d87a714c0e5a57e61bb9cdd066d205
nitya227001 <109079040+nitya227001@users.noreply.github.com>
2022-07-11 20:46:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var name1 = document.getElementById("name");
+var clgRoll = document.getElementById("clgRoll");
+var clgName = document.getElementById("clgName");
+var date = document.getElementById("date");
+var date = new date();
+window.onload -()
+window.onload -()->{
+    name1.innerText = "Nitya Sharma";
+    clgRoll.innerText = "201500455";
+    clgName.innerText = "GLA University";
+    date = date1.;
+}
